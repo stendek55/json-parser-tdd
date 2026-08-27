70e3b2f082e8b5fc130fca4a077b6aa0c630519c
stefan_s <stefan@home.pi>
2026-08-27 20:06:57 +0200

    test: erster test und zielstruktur und mögliche fehler festgelegt

--- json_parser/src/lib.rs
+++ json_parser/src/lib.rs
@@ -1,12 +1,38 @@
 //json testdatei global einbinden
 pub const TEST_JSON: &str = include_str!("tests/fixtures/valid.json");
 
+//zielstruktur für eine gültige json
+#[derive(Debug, PartialEq)]
+pub struct BenutzerAnfrage {
+    pub benutzer: String,
+    pub email: String,
+    pub alter: u32,
+}
+
+//mögliche fehler die bei wandlung eintreten könn
+#[derive(Debug, PartialEq)]
+pub enum FehlerValidierung {
+    LeereJsonDatei,
+    FalschesJsonFormat,
+    FeldFehlt(String),
+    EmailUngueltig,
+    AlterZuJung,
+}
+
 pub fn print_testdatei() {
     println!("testdatei print aus lib#########>\n{}", TEST_JSON);
 }
+
+//hauptfunktion die entwickelt werden soll
+//pub fn parse_und_validiere_json(){}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
+    fn test_eingabe_von_leerer_datei_wirft_fehler() {
+        let ergebnis = parse_und_validiere_json("");
+        assert_eq!(ergebnis, Err(FehlerValidierung::LeereJsonDatei));
+    }
 }
